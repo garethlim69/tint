38e7e635c104d865f9b3cc93e6894e039fad526a
garethlim69 <99885718+garethlim69@users.noreply.github.com>
2025-11-27 10:56:06 +0800

    Removed smtp credentials

--- Config/send_email_reminders.php
+++ Config/send_email_reminders.php
@@ -49,8 +49,8 @@ if ($user) {
       $mail->isSMTP();
       $mail->Host = 'smtp.gmail.com';
       $mail->SMTPAuth = true;
-      $mail->Username = 'garethlimjs@gmail.com';
-      $mail->Password = 'gktl jblg vkpl vnqi';
+      $mail->Username = '';
+      $mail->Password = '';
       $mail->SMTPSecure = 'tls';
       $mail->Port = 587;
 
@@ -84,3 +84,4 @@ if ($user) {
   logMessage("No email reminders enabled for $demoUserEmail.");
   echo "No email reminders enabled for $demoUserEmail.\n";
 }
+
